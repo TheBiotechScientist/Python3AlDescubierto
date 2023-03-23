2889ae3a0f50eae1b2ca79c73323da3db1bd6bb2
F. Javier Morales M <fmoralesm87@gmail.com>
2023-03-22 19:32:20 -0600

    firs commit

---
+++ practice.py
@@ -0,0 +1,22 @@
+# Conversion entre sistemas decimal, octal, binario y hexadecimal
+# %%
+hex(87)
+# >> 0x57
+oct(87)
+# >> 0o127
+bin(87)
+# >> 0b1010111
+# %%
+int(0x57)
+# >> 87
+int(0o127)
+# >> 87
+int(0b1010111)
+# >> 87
+# %% Strings
+# numero de caracteres:
+cadena = "Cadena de texto"
+type(cadena)
+# >> str
+len(cadena)
+# >> 15
